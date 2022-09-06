f087b9bcfe2156b0987fd9198e06df101df8c9bb
iampearlep <damilolaprincess@gmail.com>
2022-09-06 21:55:24 +0100

    update html, css and js

--- app.js
+++ app.js
@@ -1,5 +1,99 @@
-let element = document.getElementsByClassName('product');
 
+let change3=document.getElementById("change3")
+let click3 =document.getElementById("click3")
+let change1 =document.getElementById("change1")
+let click1 =document.getElementById("click1")
+let change2 =document.getElementById("change2")
+let click2 =document.getElementById("click2")
+
+change3.addEventListener('mouseover',function(){
+     change3.style.backgroundColor ="#2B076E"
+     change3.style.color = "#fff"
+    click3.textContent ="Start mining >"
+    click3.style.backgroundColor ="#3B82F6"
+    click3.style.width ="200px";
+    click3.style.borderRadius ="30px"
+    click3.style.color = "#fff"
+    click3.style.padding ="15px 20px 12px";
+})
+change3.addEventListener('mouseout',function(){
+    change3.style.backgroundColor ="white"
+    change3.style.color = "#000"
+    click3.textContent ="❯"
+    click3.style.color ="#3B82F6"
+click3.style.backgroundColor ="white"
+click3.style.width ="70px";
+})
+change1.addEventListener('mouseover',function(){
+    change1.style.backgroundColor ="#2B076E"
+    change1.style.color = "#fff"
+   click1.textContent ="Start mining >"
+   click1.style.backgroundColor ="#3B82F6"
+   click1.style.width = "200px"
+   click1.style.padding ="15px 20px 12px";
+   click1.style.borderRadius ="30px"
+   click1.style.color = "#fff"
+})
+change1.addEventListener('mouseout',function(){
+   change1.style.backgroundColor ="white"
+   change1.style.color = "#000"
+   click1.textContent ="❯"
+   click1.style.color ="#3B82F6"
+click1.style.backgroundColor ="white"
+click1.style.width ="70px";
+})
+change2.addEventListener('mouseover',function(){
+    change2.style.backgroundColor ="#2B076E"
+    change2.style.color = "#fff"
+   click2.textContent ="Start mining >"
+   click2.style.backgroundColor ="#3B82F6"
+   click2.style.width ="200px";
+   click2.style.borderRadius ="30px"
+   click2.style.padding ="15px 20px 12px";
+   click2.style.color = "#fff"
+})
+change2.addEventListener('mouseout',function(){
+   change2.style.backgroundColor ="white"
+   change2.style.color = "#000"
+   click2.textContent ="❯"
+   click2.style.color ="#3B82F6"
+click2.style.backgroundColor ="white"
+click2.style.width ="70px";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let element = document.getElementsByClassName('product');
+console.log(element);
 for (var i = 0; i < element.length; i++) {
   element[i].addEventListener('click', function () {
 
@@ -16,14 +110,23 @@ for (var i = 0; i < element.length; i++) {
   });
 
 }
-//let button = document.querySelector('.show-btn1');
- //console.log(button);
-//for (let i = 0; i < button.length; i++) {
-  //button[i].addEventListener('click', function (){
-   //alert('Clicked button1')
-    //this.classList.add('show-btn2');
- // });
-//}
+let button = document.getElementsByClassName('show-btn1');
+ console.log(button);
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', function (){
+    button.textContent = "Start Mining";
+   let btn = button[0];
+   while(btn) {
+    if (btn.tagName === "DIV") {
+      btn.classList.remove("show-btn2");
+    }
+    btn = btn.nextSibling;
+   }
+
+   this.classList.add('show-btn2');
+   
+ });
+} */
 
 
 
@@ -40,7 +143,55 @@ for (var i = 0; i < element2.length; i++) {
 
 } */
 
+/*let cards = document.querySelectorAll(".product");
+let span = document.querySelectorAll(".show-btn1");
 
+console.log(cards, span);
+cards.forEach(function(card){
+  card.addEventListener("mouseover", function(e){
+    card.classList.add("hover-text");
+  });
+})
+cards.forEach(function(card) {
+  cards,addEventListener("mouseout", function(e){
+    card.classList.remove("hover-text");
+  });
+});
+
+const duration = 5000;
+function render(info) {
+  let startValue = 0;
+  const item = info.target;
+  const data = parseInt(item.getAttribute("data-value"));
+  const time = Math.floor(duration / data);
+
+  let interval = setInterval(function () {
+    startValue += 1;
+    item.textContent = startValue;
+    if (startValue === data) {
+      clearInterval(interval);
+    }
+  }, [time]);
+}
+
+let options = {
+root: null,
+rootMargin: "0px",
+threshold: 1.0,
+};
+
+let myInterval = function(entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      render(entry);
+    }
+  });
+}
+*/
+//let observer = new IntersectionObserver(myInterval, options);
+//span.forEach (function(span) {
+ // observer.observe(span);
+//});
 
 
 
